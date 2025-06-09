4bd1d1ed1b6610c0a652aba4e067330935e4e0f8
petra1026 <149487838+petra1026@users.noreply.github.com>
2025-06-09 15:48:29 +0300

    pievienotas flaska funkcijas

---
+++ stili/attfkcj.js
@@ -0,0 +1,81 @@
+function showLoginModal() {
+    const modal = new bootstrap.Modal(document.
+        getElementById("loginModal"))
+        modal.show()
+}
+
+function showRegisterform() {
+    document.getElementById("loginForm").style.
+    display = "none"
+    document.getElementById("registerForm").
+    style.display = "block"
+}
+
+function showLoginForm() {
+    document.getElementById("registerForm").
+    style.display = "none"
+    document.getElementById("loginForm").style. 
+    display = "block"
+}
+
+//Ielogošanās
+document.getElementById("registerForm"). 
+addEventListener("submit", async (e)) ;{
+    e.preventDefault()
+
+    const username = document.getElementById
+    ("username").value 
+    const password = document.getElementById
+    ("password").value 
+
+    try {
+        const response = await fetch("/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({username, password}),
+        })
+
+        const data = await response.json()
+
+        if (data.success) {
+            location.reload()
+        } 
+        else {
+            alert(data.message)
+        }
+        } catch (error){
+            alert("Kļūda:" + error.message)
+        }
+    }
+
+// Reģistrācija
+document.getElementById("registerForm"). 
+addEventListener("submit", async (e)) ;{
+    e.preventDefault()
+
+    const username = document.getElementById
+    ("regUsername").value 
+    const password = document.getElementById
+    ("regPassword").value 
+
+    try {
+        const response = await fetch("/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({username, password}),
+        })
+        
+        const data = await response.json()
+        alert(data.message)
+
+        if (data.success) {
+            showLoginForm()
+        } 
+        } catch (error){
+            alert("Kļūda:" + error.message)
+        }
+    }
